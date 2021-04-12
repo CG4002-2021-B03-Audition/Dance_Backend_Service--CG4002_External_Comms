93cd3c372cd0f84e92385294f61dd803a9ed0b5f
raghav-b <raghavbhardwaj1998@gmail.com>
2021-04-12 12:34:39 +0000

    Refactor: Can now perform individual detections

--- ultra96/main.py
+++ ultra96/main.py
@@ -13,6 +13,11 @@ import time
 
 MAX_QUEUE_SIZE = 60
 
+class PacketType():
+    DANCE_PACKET = 0
+    MOVEMENT_PACKET = 1
+    EMG_PACKET = 2
+
 class Main():
     def __init__(self, listen_port=3001):
         self.ai = AI()
@@ -21,15 +26,10 @@ class Main():
         
         self.dance_window = SlidingWindow(window_size=40)
         self.movement_window = SlidingWindow(window_size=40)
-
-        #self.dancer1 = Dancer(1)
-        #self.dancer2 = Dancer(2)
-        #self.dancer3 = Dancer(3)
-        #self.dancers = [self.dancer1, self.dancer2, self.dancer3]
         
-        self.dance_queue = Queue(MAX_QUEUE_SIZE)
-        self.movement_queue = Queue(MAX_QUEUE_SIZE)
-
+        # Stores data received by zmq from laptop for each dancer
+        self.dance_data_queues = [Queue(MAX_QUEUE_SIZE), Queue(MAX_QUEUE_SIZE), Queue(MAX_QUEUE_SIZE)]
+        self.movement_data_queues = [Queue(MAX_QUEUE_SIZE), Queue(MAX_QUEUE_SIZE), Queue(MAX_QUEUE_SIZE)]
 
         print("Starting server for laptops...")
         context = zmq.Context()
@@ -38,81 +38,10 @@ class Main():
         self.laptop_socket.setsockopt(zmq.SUBSCRIBE, b'')
         print("Server for laptops started! Now waiting for laptops to connect")        
 
-        self.emg_data_queue = Queue(MAX_QUEUE_SIZE)
-
-        recv_thread = threading.Thread(target=self.dance_thread)
+        recv_thread = threading.Thread(target=self.recv_thread_func)
         recv_thread.daemon = True
         recv_thread.start()
 
-    def run(self):
-        cur_time = 0
-        first_packet = True
-        num_packets = 0
-        
-
-        while True:
-            recv_msg = self.laptop_socket.recv_pyobj()
-            parsed_msg, packet_type, dancer_id = self.parse_raw_msg(recv_msg)
-            #print(dancer_id)
-
-            if dancer_id == 1:
-                self.movement_queue.put(parsed_msg)
-                #self.dancer1.add_to_queue(parsed_msg, packet_type)
-            elif dancer_id == 2:
-                pass
-                #self.dancer2.add_to_queue(parsed_msg, packet_type)
-            elif dancer_id == 3:
-                if packet_type == 0:
-                    
-                    if first_packet == True:
-                        cur_time = time.time()
-                        first_packet = False
-                    else:
-                        self.dance_queue.put(parsed_msg)
-                        if time.time() - cur_time >= 1:
-                            #print(f"Num packets: {num_packets}")
-                            num_packets = 0
-                            cur_time = time.time()
-                    num_packets += 1
-
-                elif packet_type == 1:
-                    self.movement_queue.put(parsed_msg)
-                else:
-                    raise Exception()
-                
-            elif dancer_id == 4: # Special dancer_id reserved for EMG data
-                pass
-                #self.ext_conn.send_emg_data(parsed_msg[0])
-            else:
-                raise Exception(f"Unknown dancer ID when adding to respective queue: {dancer_id}")
-
-
-    def dance_thread(self):
-        while True:
-            if not self.dance_queue.empty():
-                dance_data = self.dance_queue.get()
-                self.dance_window.add_data(dance_data)
-
-                if self.dance_window.is_full():
-                    ai_data = self.dance_window.get_ai_data(True)
-                    time_now = time.time()
-                    prediction = self.ai.fpga_evaluate_dance(ai_data)
-                    print(f"Dance prediction: {prediction} | {time.time() - time_now}s")
-
-                    self.dance_window.advance()
-
-            if not self.movement_queue.empty():
-                movement_data = self.movement_queue.get()
-                self.movement_window.add_data(movement_data)
-
-                if self.movement_window.is_full():
-                    ai_data = self.movement_window.get_ai_data(False)
-                    #print(ai_data)
-                    time_now = time.time()
-                    prediction = self.ai.fpga_evaluate_pos(ai_data)
-                    #print(f"Movement prediction: {prediction} | {time.time() - time_now}s")
-
-                    self.movement_window.advance()
 
     def parse_raw_msg(self, raw_msg): 
         res = None
@@ -124,35 +53,115 @@ class Main():
             # c: char/uint8
             # h: sint16
             # H: uint16
-            
             if len(raw_msg) == 17: # Action packet
                 res = list(struct.unpack('<I B 6h', raw_msg))
                 start_flag = int(format(res[1], '02x')[0])
-                dancer_id = int(format(res[1], '02x')[1])
+                dancer_id = int(format(res[1], '02x')[1]) - 1
                 res.append(start_flag) # Appending start flag (Index -1)
-                #res.append(dancer_id) # Appending dancer ID (Index -1)
-                packet_type = 0
+                packet_type = PacketType.DANCE_PACKET
 
             elif len(raw_msg) == 13: # Movement packet
                 res = list(struct.unpack('<B 6h', raw_msg))
                 #movement_dir = int(format(res[0], '02x')[0])
-                dancer_id = int(format(res[0], '02x')[1])
-                #res.append(dancer_id) # Appending dancer ID (Index -2)
-                #res.append(movement_dir) # Appending movement direction (Index -1)
-                packet_type = 1
+                dancer_id = int(format(res[0], '02x')[1]) - 1
+                packet_type = PacketType.MOVEMENT_PACKET
 
             elif len(raw_msg) == 6: # EMG packet
                 res = list(struct.unpack('<i H', raw_msg))
-                dancer_id = 4
-                #res.append(dancer_id) # Appending dancer
-                packet_type = 2
+                dancer_id = 4 - 1
+                packet_type = PacketType.EMG_PACKET
 
             else:
                 raise Exception(f"Unknown packet length {len(raw_msg)}")
-
         except Exception as e:
             raise e
         return res, packet_type, dancer_id
+
+
+    def recv_thread_func(self):
+        while True:
+            recv_msg = self.laptop_socket.recv_pyobj()
+            parsed_msg, packet_type, dancer_id = self.parse_raw_msg(recv_msg)
+
+            if packet_type == PacketType.DANCE_PACKET:
+                if self.dance_data_queues[dancer_id].full():
+                    print(f"Dance data queue {dancer_id+1} full, emptying...")
+                    while not self.dance_data_queues[dancer_id].empty():
+                        x = self.dance_data_queues[dancer_id].get()
+                self.dance_data_queues[dancer_id].put(parsed_msg)
+
+            elif packet_type == PacketType.MOVEMENT_PACKET:
+                if self.movement_data_queues[dancer_id].full():
+                    print(f"Movement data queue {dancer_id+1} full, emptying...")
+                    while not self.movement_data_queues[dancer_id].empty():
+                        x = self.movement_data_queues[dancer_id].get()
+                self.movement_data_queues[dancer_id].put(parsed_msg)
+
+            elif packet_type == PacketType.EMG_PACKET:
+                pass
+                # TODO JUST SEND DATA DIRECTLY FROM HERE
+                # if self.emg_data_queue.full():
+                #     print("EMG data queue full, emptying...")
+                #     while not self.emg_data_queue.empty():
+                #         x = self.emg_data_queue.get()
+                # self.emg_data_queue.put(parsed_msg)
+
+            else:
+                raise Exception(f"Unknown PacketType: {packet_type}")                
+
+
+    def run(self):
+        # Run forever
+        while True:
+            # Iterate over all dancers
+            for dancer_id in range(0, 3): 
+                
+                # Check for dance data
+                if not self.dance_data_queues[dancer_id].empty():
+                    dance_data = self.dance_data_queues[dancer_id].get()
+
+                    # Add data to sliding window to prepare for detections
+                    self.dance_window.add_data(dance_data, dancer_id)
+
+                    # Once sliding window is full, we can perform detections using the AI
+                    if self.dance_window.is_full():
+                        # Get correct format of AI data 
+                        ai_data = self.dance_window.get_ai_data(dancer_id, is_move=False)
+                        
+                        # Perform prediction of dance move
+                        cur_time = time.time()
+                        prediction = self.ai.fpga_evaluate_dance(ai_data)
+                        print(f"Dancer {dancer_id+1} Dance: {prediction} | {time.time() - time_now}s")
+
+                        # Store prediction in dance_filter_window
+                        # TODO
+
+                        # Check if filter window is full
+
+                        # Get maximum element from filter window 
+
+                        # Advance sliding window since a detection has finished
+                        self.dance_window.advance()
+
+                # Check for movement data
+                if not self.movement_data_queues[dancer_id].empty():
+                    movement_data = self.movement_data_queues[dancer_id].get()
+
+                    # Add data to sliding window to prepare for detections
+                    if self.movement_window.is_full():
+                        # Get correct format of AI data
+                        ai_data = self.movement_window.get_ai_data(dancer_id, is_move=True)
+
+                        # Perform prediction of movement
+                        cur_time = time.time()
+                        prediction = self.ai.fpga_evaluate_pos(ai_data)
+                        print(f"Dancer {dancer_id+1} Movement: {prediction} | {time.time() - time_now}s")
+
+                        # Store prediction in movement_filter_window
+                        # TODO
+
+                        # Advance sliding window since a detection has finished
+                        self.movement_window.advance()
 
 
 if __name__ == "__main__":
--- ultra96/test.py
+++
@@ -1,7 +0,0 @@
-x = [1,2,3]
-
-def func(y):
-    y[0] = 0
-
-func(x)
-print(x)
